bc98e4b053330ed5e87ac40fc1a2ec9856b89460
OlexandrOnokalo <Onoka_nd96@student.itstep.org>
2025-06-11 19:51:57 +0300

    HW 02

--- 01_IntroToDotNet/Program.cs
+++ 01_IntroToDotNet/Program.cs
@@ -1,115 +1,120 @@
-﻿namespace _01_IntroToDotNet
+﻿using System; // Підключення стандартної бібліотеки для роботи з консоллю та іншими базовими можливостями
+
+class Program
 {
-    using System; 
-
-    class Program
+    static void Main()
     {
-        static void Main()
+        /*Завдання 1 Виведіть на екран цитату Б'ярна Страуструпа: It's easy to win forgiveness
+                     * for being wrong; being right is what gets you into real trouble. Приклад виводу:
+                     * It's easy to win forgiveness for being wrong;  being right is what gets you into
+                     * real trouble. Bjarne Stroustrup*/
+        
+        Console.WriteLine("It's easy to win forgiveness for being wrong;\nbeing right is what gets you into real trouble.\nBjarne Stroustrup");
+
+        /*Завдання 2 Користувач вводить з клавіатури п'ять чисел. Необхідно знайти суму чисел, максимум і мінімум з п'яти чисел, добуток чисел. Результат обчислень вивести на екран.*/
+        int[] numbers = new int[5];
+        Console.WriteLine("Enter 5 numbers:");
+
+        for (int i = 0; i < 5; i++)
         {
-            
-            Console.WriteLine("It's easy to win forgiveness for being wrong;");
-            Console.WriteLine("being right is what gets you into real trouble.");
-            Console.WriteLine("Bjarne Stroustrup\n"); 
+            Console.Write($"Number {i + 1}: "); 
+            numbers[i] = int.Parse(Console.ReadLine()!); 
+        }
 
-            
-            int[] numbers = new int[5]; 
-            Console.WriteLine("Enter 5 numbers:");
+        
+        int sum = 0, product = 1, min = numbers[0], max = numbers[0];
 
-            for (int i = 0; i < 5; i++)
+        foreach (int num in numbers) 
+        {
+            sum += num;        
+            product *= num;    
+            if (num < min) min = num; 
+            if (num > max) max = num; 
+        }
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Product: {product}");
+        Console.WriteLine($"Min: {min}");
+        Console.WriteLine($"Max: {max}\n");
+
+        /*Завдання 3 Користувач з клавіатури вводить шестизначне число. Необхідно перевернути число і відобразити результат. Наприклад, якщо введено 341256, результат 652143.без масива Array*/
+
+
+        Console.Write("Enter a six-digit number: ");
+        string input = Console.ReadLine()!;
+        Console.WriteLine($"Resault: {input[5]}{input[4]}{input[3]}{input[2]}{input[1]}{input[0]}");
+
+        /*Завдання 4 Користувач вводить з клавіатури межі числового діапазону.
+             * Потрібно показати усі числа Фібоначчі з цього 1діапазону.
+             * Числа Фібоначчі — елементи числової послідовності, у якій перші два числа дорівнюють
+             * 0 і 1, а кожне наступне число дорівнює сумі двох попередніх чисел. Наприклад,
+             * якщо вказано діапазон 0–20, результат буде: 0, 1, 1, 2, 3, 5, 8, 13.*/
+
+        Console.Write("Enter range start: ");
+        int rangeStart = int.Parse(Console.ReadLine()!); 
+
+        Console.Write("Enter range end: ");
+        int rangeEnd = int.Parse(Console.ReadLine()!); 
+
+        int a = 0, b = 1;
+        Console.Write("Fibonacci numbers in range: ");
+        while (a <= rangeEnd)
+        {
+            if (a >= rangeStart)
+                Console.Write($"{a} "); 
+            int temp = a + b; 
+            a = b; 
+            b = temp;
+        }
+        Console.WriteLine("\n");
+
+        /*Завдання 5 Дано цілі додатні числа A і B (A < B).
+             * Вивести усі цілі числа від A до B включно; кожне число має виводитися у новому рядку;
+             * при цьому кожне число має виводитися у кількість разів, рівну його значенню. Наприклад:
+             * якщо А = 3, а В = 7, то програма має сформувати в консолі такий висновок: 3 3 3 4 4 4 4 5 5 5 5 5 6 6 6 6 6 6 7 7 7 7 7 7 7*/
+        Console.Write("Enter A (A < B): ");
+        int A = int.Parse(Console.ReadLine()!);
+
+        Console.Write("Enter B: ");
+        int B = int.Parse(Console.ReadLine()!);
+
+        for (int i = A; i <= B; i++) 
+        {
+            for (int j = 0; j < i; j++) 
             {
-                Console.Write($"Number {i + 1}: "); 
-                numbers[i] = int.Parse(Console.ReadLine()); 
+                Console.Write($"{i} ");
             }
+            Console.WriteLine(); 
+        }
+        Console.WriteLine();
 
-            
-            int sum = 0, product = 1, min = numbers[0], max = numbers[0];
+        /*Завдання 6 Користувач з клавіатури вводить довжину лінії, символ заповнювач,
+             * напрямок лінії (горизонтальна, вертикальна). Програма відображає лінію по заданих параметрах.
+             * Наприклад: +++++. Параметри лінії: горизонтальна лінія, довжина дорівнює п'яти, символ заповнювач +.*/
 
-            foreach (int num in numbers) 
-            {
-                sum += num;        
-                product *= num;    
-                if (num < min) min = num; 
-                if (num > max) max = num; 
-            }
 
-            
-            Console.WriteLine($"Sum: {sum}");
-            Console.WriteLine($"Product: {product}");
-            Console.WriteLine($"Min: {min}");
-            Console.WriteLine($"Max: {max}\n");
+        Console.Write("Enter line length: ");
+        int length = int.Parse(Console.ReadLine()!); 
 
-            
-            Console.Write("Enter a six-digit number: ");
-            string input = Console.ReadLine(); 
-            char[] reversed = input.ToCharArray(); 
-            Array.Reverse(reversed);
-            string result = new string(reversed); 
-            Console.WriteLine($"Reversed number: {result}\n");
+        Console.Write("Enter fill symbol: ");
+        char fill = Console.ReadLine()![0]; 
 
-            
-            Console.Write("Enter range start: ");
-            int rangeStart = int.Parse(Console.ReadLine()); 
+        Console.Write("Enter direction (horizontal/vertical): ");
+        string direction = Console.ReadLine()!; 
 
-            Console.Write("Enter range end: ");
-            int rangeEnd = int.Parse(Console.ReadLine()); 
-
-            int a = 0, b = 1; 
-            Console.Write("Fibonacci numbers in range: ");
-            while (a <= rangeEnd)
-            {
-                if (a >= rangeStart)
-                    Console.Write($"{a} "); 
-                int temp = a + b; 
-                a = b; 
-                b = temp;
-            }
-            Console.WriteLine("\n");
-
-            
-            Console.Write("Enter A (A < B): ");
-            int A = int.Parse(Console.ReadLine());
-
-            Console.Write("Enter B: ");
-            int B = int.Parse(Console.ReadLine());
-
-            for (int i = A; i <= B; i++) 
-            {
-                for (int j = 0; j < i; j++) 
-                {
-                    Console.Write($"{i} ");
-                }
-                Console.WriteLine(); 
-            }
+        if (direction.ToLower() == "horizontal") 
+        {
+            for (int i = 0; i < length; i++)
+                Console.Write(fill); 
             Console.WriteLine();
-
-            
-            Console.Write("Enter line length: ");
-            int length = int.Parse(Console.ReadLine()); 
-
-            Console.Write("Enter fill symbol: ");
-            char fill = Console.ReadLine()[0];
-
-            Console.Write("Enter direction (horizontal/vertical): ");
-            string direction = Console.ReadLine(); 
-
-            if (direction.ToLower() == "horizontal") 
-            {
-                for (int i = 0; i < length; i++)
-                    Console.Write(fill); 
-                Console.WriteLine();
-            }
-            else if (direction.ToLower() == "vertical") 
-            {
-                for (int i = 0; i < length; i++)
-                    Console.WriteLine(fill);
-            }
-            else
-            {
-                Console.WriteLine("Invalid direction."); 
-            }
+        }
+        else if (direction.ToLower() == "vertical") 
+        {
+            for (int i = 0; i < length; i++)
+                Console.WriteLine(fill); 
+        }
+        else
+        {
+            Console.WriteLine("Invalid direction."); 
         }
     }
-
-
-
 }
--- 02_Array/Program.cs
+++ 02_Array/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _02_Array
+﻿using System;
+
+namespace _02_Array
 {
     internal class Program
     {
@@ -58,7 +60,7 @@
 
             Console.WriteLine("Task 2:");
             Console.WriteLine("Enter number: ");
-            int n = int.Parse(Console.ReadLine())!;
+            int n = int.Parse(Console.ReadLine()!);
             int count = 0;
             foreach (int elem in array)
             {
@@ -85,22 +87,158 @@
 
             int[] A = new int[5];
             int row = 3, col = 4;
-            int[,] B = new int[row,col];
+            double[,] B = new double[row, col];
+            Random random = new Random();
+
             
-            for (int i = 0;i < 5; i++)
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine($"Enter {i+1} number: ");
-                A[i] = int.Parse(Console.ReadLine())!;
+                Console.WriteLine($"Enter {i + 1} number: ");
+                A[i] = int.Parse(Console.ReadLine()!);
             }
 
+            
             for (int i = 0; i < row; i++)
             {
                 for (int j = 0; j < col; j++)
                 {
-                    int[]
+                    B[i, j] = Math.Round(random.Next(100) + random.NextDouble(), 2);
                 }
             }
+
             
+            Console.Write("Array A: ");
+            foreach (int val in A)
+                Console.Write(val + " ");
+            Console.WriteLine();
+
+            
+            Console.WriteLine("Array B:");
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write(B[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            
+            double max = A[0];
+            double min = A[0];
+            double sum = 0;
+            double product = 1;
+            int sumEvenA = 0;
+            double sumOddColsB = 0;
+
+            
+            foreach (int val in A)
+            {
+                if (val > max) max = val;
+                if (val < min) min = val;
+                sum += val;
+                product *= val;
+                if (val % 2 == 0) sumEvenA += val;
+            }
+
+            
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    double val = B[i, j];
+                    if (val > max) max = val;
+                    if (val < min) min = val;
+                    sum += val;
+                    product *= val;
+                    if (j % 2 == 1) 
+                        sumOddColsB += val;
+                }
+            }
+            sum = Math.Round(sum, 2);
+            product = Math.Round(product, 2);
+            sumOddColsB = Math.Round(sumOddColsB, 2);
+
+
+            Console.WriteLine($"Max element: {max}");
+            Console.WriteLine($"Min element: {min}");
+            Console.WriteLine($"Sum of all elements: {sum}");
+            Console.WriteLine($"Product of all elements: {product}");
+            Console.WriteLine($"Sum of even elements in A: {sumEvenA}");
+            Console.WriteLine($"Sum of elements in odd columns of B: {sumOddColsB}");
+
+            //    Завдання 4
+            //Дано двовимірний масив розміром 5×5, заповне -
+            //ний випадковими числами з діапазону від –100 до 100.
+            //Визначити суму елементів масиву, розташованих між
+            //мінімальним і максимальним елементами.
+
+            Console.WriteLine("Task 4:");
+
+            int[,] array2D = new int[5, 5];
+            Random rand = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    array2D[i, j] = rand.Next(100);
+                    Console.Write(array2D[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            
+            int[] flatArray = new int[25];
+            int index = 0;
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                    flatArray[index++] = array2D[i, j];
+
+            int minIndex = 0, maxIndex = 0;
+            for (int i = 1; i < flatArray.Length; i++)
+            {
+                if (flatArray[i] < flatArray[minIndex])
+                    minIndex = i;
+                if (flatArray[i] > flatArray[maxIndex])
+                    maxIndex = i;
+            }
+
+            int start = Math.Min(minIndex, maxIndex);
+            int end = Math.Max(minIndex, maxIndex);
+            int sumBetween = 0;
+            for (int i = start + 1; i < end; i++)
+            {
+                sumBetween += flatArray[i];
+            }
+
+            Console.WriteLine($"Sum of elements between min and max: {sumBetween}");
+
+
+            //    Завдання 5:
+            //Визначити кількість елементів в заданому масиві, що відрізняються
+            //від мінімального на 5.
+
+            Console.WriteLine("Task 5:");
+
+            int minVal = array[0];
+            foreach (int val in array)
+            {
+                if (val < minVal)
+                    minVal = val;
+            }
+
+            int diffCount = 0;
+            foreach (int val in array)
+            {
+                if (Math.Abs(val - minVal) == 5)
+                    diffCount++;
+            }
+
+            Console.WriteLine($"Number of elements differing from min ({minVal}) by 5: {diffCount}");
+
+
+
 
         }
     }
