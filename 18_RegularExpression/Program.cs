8537a291421b2033141f732290db05277f5ee0af
OlexandrOnokalo <Olexandr.onokalo@gmail.com>
2025-07-16 17:55:34 +0300

    LAB 18

---
+++ 18_RegularExpression/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _18_RegularExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = File.ReadAllText("text.txt");
+            Console.WriteLine(jsonString);
+
+            MatchCollection coll = Regex.Matches(jsonString, @"\d+\.\d+|\d+\,\d+");
+            foreach (Match match in coll)
+            {
+                Console.WriteLine($"Index = {match.Index}. Value : {match.Value}");
+            }
+
+            Console.WriteLine();
+            int index = coll.Count;
+            double[] numb = new double[index];
+            for (int i = 0; i < index; i++)
+            {
+                numb[i] = double.Parse(coll[i].Value);
+            }
+
+            foreach (double num in numb) {
+                Console.WriteLine(num);
+            }
+
+
+
+
+
+
+        }
+    }
+}
